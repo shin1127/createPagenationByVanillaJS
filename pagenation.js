6fc384f4e0aac20c54d9e2a15cd77d9c92ae2815
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-07-24 11:09:14 +0900

    Update pagenation.js

--- pagenation.js
+++ pagenation.js
@@ -1,12 +1,18 @@
-
 const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const perPage = 2;
 const getPage = (p) => {
   
-  document.write(data[2*p - 2]);
-  document.write(" ");
-  document.write(data[2*p - 1]);
-  document.write(" ");
+  console.log(data[2*p - 2]);
+  console.log(data[2*p - 1]);
+  
+  hoge = []
+  
+  // ここ拡張for文にできるね？
+  hoge.push(data[2*p - 2]);
+  hoge.push(data[2*p - 1]);
+  
+  return hoge;
+  
   };
   
   
